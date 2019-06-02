fb39f1df10c0111aad218d979bbf518bdc899047
danii <wersfit@gmail.com>
2019-06-02 12:18:58 +0300

    almost all opengl and glfw logic is wrapped in GameRenderer

--- src/main/java/Game/Graphics/GameRenderer.java
+++ src/main/java/Game/Graphics/GameRenderer.java
@@ -2,15 +2,31 @@ package Game.Graphics;
 
 import Game.Logic.TexturedEntity;
 import org.joml.Matrix3f;
+import org.joml.Vector2f;
 import org.joml.Vector3f;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.system.MemoryStack;
+
 import java.io.File;
 
+import java.nio.IntBuffer;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.glfw.GLFW.GLFW_CURSOR_HIDDEN;
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class GameRenderer {
+
+   public static long window;
+   public static int H = 800;
+   public static int W = 920;
 
    public static Map<Integer, Texture> textureMap;
 
@@ -45,6 +61,40 @@ public class GameRenderer {
 
       glEnable(GL_TEXTURE_2D);
       bindTexture(entity.getTextureID());
+      glBegin(GL_QUADS);
+
+      glTexCoord2f(0, 0);
+      glVertex2f(p1.x, p1.y);
+
+      glTexCoord2f(0, 1);
+      glVertex2f(p2.x, p2.y);
+
+      glTexCoord2f(1, 1);
+      glVertex2f(p3.x, p3.y);
+
+      glTexCoord2f(1, 0);
+      glVertex2f(p4.x, p4.y);
+
+      glEnd();
+
+      glDisable(GL_TEXTURE_2D);
+   }
+
+   public static void renderEntity(Vector3f pos, Vector3f cameraPos, float r, float fi, int textureID) {
+      Matrix3f rotation_matrix = new Matrix3f();
+      rotation_matrix.rotate(fi, 0, 0, 1, rotation_matrix);
+
+      Vector3f p1 = Camera.transform(pos, cameraPos, new Vector3f(pos.x - r / 2.0f, pos.y + r / 2.0f, 0), rotation_matrix);
+      Vector3f p2 = Camera.transform(pos, cameraPos, new Vector3f(pos.x + r / 2.0f, pos.y + r / 2.0f, 0), rotation_matrix);
+      Vector3f p3 = Camera.transform(pos, cameraPos, new Vector3f(pos.x + r / 2.0f, pos.y - r / 2.0f, 0), rotation_matrix);
+      Vector3f p4 = Camera.transform(pos, cameraPos, new Vector3f(pos.x - r / 2.0f, pos.y - r / 2.0f, 0), rotation_matrix);
+
+      glShadeModel(GL_SMOOTH);
+
+      glColor3f(1f, 1.0f, 1f);
+
+      glEnable(GL_TEXTURE_2D);
+      bindTexture(textureID);
       glBegin(GL_QUADS);
 
       glTexCoord2f(0, 0);
@@ -108,6 +158,102 @@ public class GameRenderer {
 
       glEnd();
       glDisable(GL_TEXTURE_2D);
+   }
+
+   public static void init() {
+
+      GLFWErrorCallback.createPrint(System.err).set();
+
+      if (!glfwInit())
+         throw new IllegalStateException("Unable to initialize GLFW");
+
+      glfwDefaultWindowHints();
+      glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+
+
+      window = glfwCreateWindow(W, H, "test", NULL, NULL);
+      if (window == NULL)
+         throw new RuntimeException("Failed to create the GLFW window");
+
+      glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+         if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+            glfwSetWindowShouldClose(window, true);
+         if (key == GLFW_KEY_UP) {
+            Camera.zoomIn();
+         }
+         if (key == GLFW_KEY_DOWN) {
+            Camera.zoomOut();
+         }
+      });
+
+      // Get the thread stack and push a new frame
+      try (MemoryStack stack = stackPush()) {
+         IntBuffer pWidth = stack.mallocInt(1);
+         IntBuffer pHeight = stack.mallocInt(1);
+
+         glfwGetWindowSize(window, pWidth, pHeight);
+
+         GLFWVidMode vidMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+         glfwSetWindowPos(
+                 window,
+                 (vidMode.width() - pWidth.get(0)) / 2,
+                 (vidMode.height() - pHeight.get(0)) / 2
+         );
+      } // the stack frame is popped automatically
+
+      glfwMakeContextCurrent(window);
+
+      glfwSwapInterval(1);
+
+      glfwShowWindow(window);
+
+      GL.createCapabilities();
+      Camera.updateAspectRatio(W, H);
+
+      glEnable(GL_BLEND);
+      glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+      glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+   }
+
+   public static void destroy() {
+      glfwFreeCallbacks(window);
+      glfwDestroyWindow(window);
+
+      glfwTerminate();
+      glfwSetErrorCallback(null).free();
+   }
+
+   public static boolean windowShouldClose() {
+      return glfwWindowShouldClose(GameRenderer.window);
+   }
+
+   public static int getKeyState(int key) {
+       return glfwGetKey(window, key);
+   }
+
+   public static void clearWindow() {
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+      glClearColor(1, 1, 1, 1);
+      glColor4f(1, 1, 1, 1f);
+   }
+
+   public static void updateScreen() {
+      glfwSwapBuffers(GameRenderer.window); // swap the color buffers
+   }
+
+   //
+   public static Vector2f getCursorPos() {
+      double[] cursor_x = new double[1], cursor_y = new double[1];
+
+      glfwGetCursorPos(GameRenderer.window, cursor_x, cursor_y);
+      return new Vector2f((float) ((cursor_x[0] - GameRenderer.W / 2.0d) / GameRenderer.W * 2.0f),
+              (float) ((GameRenderer.H / 2.0d - cursor_y[0]) / GameRenderer.H * 2.0f));
    }
 
 }
--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -3,45 +3,23 @@ package Main;
 
 import Game.Graphics.Camera;
 import Game.Graphics.GameRenderer;
-import Game.Graphics.Texture;
 import Game.Logic.*;
-import Game.Network.ServerService;
 import Game.Network.Server;
+import Game.Network.ServerService;
 import org.joml.Vector2f;
-import org.lwjgl.glfw.GLFWErrorCallback;
-import org.lwjgl.glfw.GLFWVidMode;
-import org.lwjgl.opengl.GL;
-import org.lwjgl.system.MemoryStack;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.IntBuffer;
 
-import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryStack.stackPush;
-import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class Main {
-    private static int H = 800;
-    private static int W = 920;
-    float cursor_size = 0.02f;
-    float heart_size = 0.09f;
     GameWorld gameWorld;
-    float scale = 0.099999999f;
-    Entity field;
-    Entity field2;
     boolean isOnline = false;
-    private float FPS = 60;
-    private long millis_per_frame = (long) ((1.0f / 30.0f) * 1000.0f);
     private Player player;
-    private Texture cursor_tex;
     Vector2f cursor_pos;
 
-    // The window handle
-    private long window;
     private BufferedReader input;
     private ServerService clientService;
     private Server server;
@@ -51,90 +29,17 @@ public class Main {
     }
 
     public void run() {
-        init();
-        loop();
-
-        glfwFreeCallbacks(window);
-        glfwDestroyWindow(window);
-
-        glfwTerminate();
-        glfwSetErrorCallback(null).free();
-    }
-
-    private void init() {
-
-        GLFWErrorCallback.createPrint(System.err).set();
-
-        if (!glfwInit())
-            throw new IllegalStateException("Unable to initialize GLFW");
-
-        glfwDefaultWindowHints();
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-
-
-        window = glfwCreateWindow(W, H, "test", NULL, NULL);
-        if (window == NULL)
-            throw new RuntimeException("Failed to create the GLFW window");
-
-        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
-                glfwSetWindowShouldClose(window, true);
-            if (key == GLFW_KEY_UP) {
-                Camera.zoomIn();
-                scale *= 1.1;
-            }
-            if (key == GLFW_KEY_DOWN) {
-                Camera.zoomOut();
-                scale /= 1.1;
-            }
-        });
-
-        // Get the thread stack and push a new frame
-        try (MemoryStack stack = stackPush()) {
-            IntBuffer pWidth = stack.mallocInt(1);
-            IntBuffer pHeight = stack.mallocInt(1);
-
-            glfwGetWindowSize(window, pWidth, pHeight);
-
-            GLFWVidMode vidMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-            glfwSetWindowPos(
-                    window,
-                    (vidMode.width() - pWidth.get(0)) / 2,
-                    (vidMode.height() - pHeight.get(0)) / 2
-            );
-        } // the stack frame is popped automatically
-
-        glfwMakeContextCurrent(window);
-
-        glfwSwapInterval(1);
-
-        glfwShowWindow(window);
-
-        GL.createCapabilities();
-        Camera.updateAspectRatio(W, H);
-
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
-
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-
-        GameRenderer.loadTextures();
-
-
-        cursor_pos = new Vector2f();
-
-
         initGame();
+        GameRenderer.init();
+        GameRenderer.loadTextures();
+        loop();
+        GameRenderer.destroy();
     }
 
 
     private void initGame() {
-
+        cursor_pos = new Vector2f();
+        gameWorld = new GameWorld();
         gameWorld = new GameWorld();
         player = new Player(5, 1, 1f, 5, 2);
 
@@ -161,7 +66,7 @@ public class Main {
         processInput();
         last_time = start_time;
 
-        while (!glfwWindowShouldClose(window)) {
+        while (!GameRenderer.windowShouldClose()) {
 
             start_time = System.currentTimeMillis();
 
@@ -179,23 +84,17 @@ public class Main {
     }
 
     void render() {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
-        glClearColor(1, 1, 1, 1);
 
-        glColor4f(1, 1, 1, 1f);
-
-        glMatrixMode(GL_MODELVIEW);
-        glPushMatrix();
-
+        GameRenderer.clearWindow();
         gameWorld.getEntities().forEach(entity -> {
             if (entity instanceof TexturedEntity) {
-                GameRenderer.renderEntity((TexturedEntity) entity, player.getPos());
+                GameRenderer.renderEntity(entity.getPos(), player.getPos(), entity.getR(), entity.getFi(), ((TexturedEntity) entity).getTextureID());
+                //TODO this parameters can be overridden from network interface because now it is just numbers
             }
         });
 
         GameRenderer.renderHUD(cursor_pos.x, cursor_pos.y, player.getHP());
-
-        glfwSwapBuffers(window); // swap the color buffers
+        GameRenderer.updateScreen();
     }
 
 
@@ -203,19 +102,19 @@ public class Main {
     void processInput() {
         byte direction = 0;
 
-        int state = glfwGetKey(window, GLFW_KEY_W);
+        int state = GameRenderer.getKeyState(GLFW_KEY_W);
         if (state == GLFW_PRESS) {
             direction |= Player.UP;
         }
-        state = glfwGetKey(window, GLFW_KEY_S);
+        state = GameRenderer.getKeyState(GLFW_KEY_S);
         if (state == GLFW_PRESS) {
             direction |= Player.DOWN;
         }
-        state = glfwGetKey(window, GLFW_KEY_A);
+        state = GameRenderer.getKeyState(GLFW_KEY_A);
         if (state == GLFW_PRESS) {
             direction |= Player.LEFT;
         }
-        state = glfwGetKey(window, GLFW_KEY_D);
+        state = GameRenderer.getKeyState(GLFW_KEY_D);
         if (state == GLFW_PRESS) {
             direction |= Player.RIGHT;
         }
@@ -223,33 +122,26 @@ public class Main {
         player.move(direction);
         if (isOnline) clientService.moveNude(direction);
 
-        state = glfwGetKey(window, GLFW_KEY_SPACE);
+        state = GameRenderer.getKeyState(GLFW_KEY_SPACE);
         if (state == GLFW_PRESS) {
             player.shot(gameWorld);
         }
 
-        state = glfwGetKey(window, GLFW_KEY_R);
+        state = GameRenderer.getKeyState(GLFW_KEY_R);
         if (state == GLFW_PRESS) {
             initGame();
         }
 
-        double[] cursor_x = new double[1], cursor_y = new double[1];
-
-        glfwGetCursorPos(window, cursor_x, cursor_y);
-
-        float norm_x, norm_y;
-
-        norm_x = (float) ((cursor_x[0] - W / 2) / W * 2.0f);
-        norm_y = (float) ((H / 2 - cursor_y[0]) / H * 2.0f);
+        Vector2f cursorPos = GameRenderer.getCursorPos();
 
         // crutch for sending angle though network
-        int angle = (int) (10000 * Math.atan2(norm_y, norm_x * Camera.getAspectRatio()));
+        int angle = (int) (10000 * Math.atan2(cursorPos.y, cursorPos.x * Camera.getAspectRatio()));
 
         player.setDeciFi(angle);
         if (isOnline) clientService.angleNude(angle);
 
-        cursor_pos.x = norm_x;
-        cursor_pos.y = norm_y;
+        cursor_pos.x = cursorPos.x;
+        cursor_pos.y = cursorPos.y;
     }
 
 
@@ -257,7 +149,7 @@ public class Main {
     private class ConsoleEvent extends Thread {
         @Override
         public void run() {
-            while (!glfwWindowShouldClose(window)) {
+            while (!GameRenderer.windowShouldClose()) {
                 String string;
 
                 try {
@@ -265,6 +157,7 @@ public class Main {
 
                     if (string.equals("/server start")) {
                         server = new Server(gameWorld);
+                        System.out.println("Server started");
                     } else if (string.contains("/connect")) {
                         String[] str = string.split(" ");
                         String[] s = str[1].split(":");
